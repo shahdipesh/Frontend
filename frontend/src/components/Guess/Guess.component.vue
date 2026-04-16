0043ad18e815cd198f44e7df98f5c8e8ce949e46
dipeshasd <dipesh.sah@hotmail.com>
2026-04-16 18:36:27 -0500

    final

--- frontend/src/components/Guess/Guess.component.vue
+++ frontend/src/components/Guess/Guess.component.vue
@@ -4,9 +4,15 @@ import { io } from "socket.io-client";
 import Button from 'primevue/button'
 import axios from "axios";
 import socket from '../../router/socket'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
 let isImposter = ref(false)
 let word = ref('')
 let isTurnToGuess = ref(false);
+let isGameOver = ref(false)
+let msg = ref('')
+let isOwner = ref(false)
 const backendUrl = import.meta.env.VITE_BACKEND_URL
 
 onMounted(() => {
@@ -16,7 +22,14 @@ onMounted(() => {
         socket.emit('JOIN_GAME', { gameId, userId })
     }
 
-    // backend uses events.IMPOSTER; keep both just in case
+    axios.get(`${backendUrl}/isOwner`,{
+        params: { userId, gameId}
+    }).then(res=>{
+        if(res.data.isOwner){
+            isOwner.value = true
+        }
+    })
+
     socket.on('IMPOSTER', () => {
         isImposter.value = true
     })
@@ -30,7 +43,12 @@ onMounted(() => {
     })
 
     socket.on('ROUND_OVER', () => {
-        alert("ROUND OVER")
+        msg.value = 'GAME OVER!. GUESS WHO IS THE IMPOSTER'
+        isGameOver.value = true
+    })
+
+    socket.on('PLAY_AGAIN', () => {
+        router.push('/join-room')
     })
 })
 
@@ -43,15 +61,37 @@ let guess = async () => {
             isTurnToGuess.value = false;
         }
     }).catch(e => {
-            alert(e)
-        })
+        res.send(e)
+    })
+}
+
+let playAgain = async () => {
+    axios.post(`${backendUrl}/game/restart`, {
+        gameId: localStorage.getItem('gameId'),
+        userId: localStorage.getItem('userId')
+    }).then(res => {
+        if (res.status == 200) {
+            msg.value = ''
+            router.push('/join-room')
+        }
+    }).catch(e => {
+        res.send(e)
+    })
+
 }
 
 </script>
 
 <template>
+    {{ isImposter }}
     <h1 class="container flex flex-column gap-3">
-        {{ isImposter ? "You are the imposter" : `Your word is: ${word}` }}
-        <Button v-if="isTurnToGuess" label="Guess" @click="guess" />
+        <span class="container flex flex-column gap-3" v-if="isGameOver">
+            {{ msg }}
+            <Button v-if="isGameOver && isOwner" label="Play Again" @click="playAgain" />
+        </span>
+        <span class="container flex flex-column gap-3" v-else>
+            {{ isImposter ? "You are the imposter" : `Your word is: ${word}` }}
+            <Button v-if="isTurnToGuess" label="Guess" @click="guess" />
+        </span>
     </h1>
 </template>
